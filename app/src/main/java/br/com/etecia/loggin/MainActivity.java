296d7a1b9bbff44f21a39ae9e13e7d9db57f4405
gabrielly alves <gabrielly.alves2@etec.sp.gov.br>
2019-04-02 22:29:44 -0300

    alteração do projeto

--- app/src/main/java/br/com/etecia/loggin/MainActivity.java
+++ app/src/main/java/br/com/etecia/loggin/MainActivity.java
@@ -2,6 +2,9 @@ package br.com.etecia.loggin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      Button buttonLogin = findViewById(R.id.buttonLogin);
+      buttonLogin.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              TextView textUsername = findViewById(R.id.Username);
+              TextView textPassword = findViewById(R.id.password);
+              String Username = textUsername.getText().toString();
+              String password = textPassword.getText().toString();
+          }
+      });
+
     }
 }
